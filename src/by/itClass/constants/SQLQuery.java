b467bafcdf4728f38365f809006a540c02ca5c35
D-CURT <36221315+D-CURT@users.noreply.github.com>
2018-07-02 22:28:15 +0300

    Usable version;

--- src/by/itClass/constants/SQLQuery.java
+++ src/by/itClass/constants/SQLQuery.java
@@ -4,7 +4,7 @@ public final class SQLQuery {
     public static final String SELECT_FOUND_USERS = "SELECT * FROM User WHERE login=? AND password=?";
     public static final String SELECT_FOUND_LOGIN = "SELECT * FROM User WHERE login=?";
 
-    public static final String INSERT_USERS = "INSERT INTO User(login, password, fistName, eMail) values(?,?,?,?)";
+    public static final String INSERT_USERS = "INSERT INTO User (login, password, firstName, eMail) VALUES (?, ?, ?, ?)";
 
     public static final int LOGIN_POSITION = 1;
     public static final int PASSWORD_POSITION = 2;
