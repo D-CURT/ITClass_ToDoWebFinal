babc023ab21103686e78914a294610822d330e60
D-CURT <36221315+D-CURT@users.noreply.github.com>
2018-07-06 01:53:52 +0300

    In web/DB/ db version added; TaskController class updated;

--- src/by/itClass/controllers/TaskController.java
+++ src/by/itClass/controllers/TaskController.java
@@ -7,22 +7,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 import by.itClass.Section.SectionTask;
 import by.itClass.constants.Constants;
 import by.itClass.factory.TaskFactory;
-import by.itClass.impl.UserDBImplementation
 import by.itClass.interfaces.ITaskDAO;
-import by.itClass.model.beans.Task;
 import by.itClass.model.beans.User;
 
 @WebServlet("/task")
 public class TaskController extends AbstractController {
 
     @Override
-    public void performTask(HttpServletRequest request, HttpServletResponse response) throws SecurityException, IOException, ServletException {
+    public void performTask(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         HttpSession session = request.getSession();
         User user = (User) session.getAttribute(Constants.KEY_USER);
         String paramList = (String) session.getAttribute(Constants.KEY_PARAM_LIST);
@@ -30,7 +26,7 @@ public class TaskController extends AbstractController {
         if (paramList == null) paramList = "today";
         SectionTask sectionTask = (SectionTask) TaskFactory.getKindSectionTask(paramList);
         try {
-            session.setAttribute(taskDAO.getTasks(user, sectionTask));
+            session.setAttribute(Constants.KEY_PARAM_LIST, taskDAO.getTasks(user, sectionTask));
         } catch (Exception e) {
             e.printStackTrace();
         }
