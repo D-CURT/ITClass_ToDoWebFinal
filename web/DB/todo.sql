63a2e140992772447978f1d25e90f6b659ed3704
D-CURT <36221315+D-CURT@users.noreply.github.com>
2018-08-03 13:28:50 +0300

    DB copy updated;

--- web/DB/todo.sql
+++ web/DB/todo.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Хост: 127.0.0.1
--- Время создания: Июл 27 2018 г., 00:50
+-- Время создания: Авг 03 2018 г., 14:25
 -- Версия сервера: 5.5.25
 -- Версия PHP: 5.3.13
 
@@ -35,16 +35,17 @@ CREATE TABLE IF NOT EXISTS `task` (
   `flagFix` int(11) NOT NULL DEFAULT '0',
   `flagRecycle` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
 
 --
 -- Дамп данных таблицы `task`
 --
 
 INSERT INTO `task` (`id`, `idUser`, `titleTask`, `contentTask`, `dateTask`, `flagFix`, `flagRecycle`) VALUES
-(1, 1, 'First Task', '111', '2018-07-26', 0, 0),
-(2, 1, 'Second task', '222', '2018-07-27', 0, 0),
-(3, 1, 'Theard task', '333', '2018-07-28', 0, 0);
+(1, 1, 'First task', '111', '2018-08-03', 0, 0),
+(2, 1, 'Second task', '222', '2018-08-04', 0, 0),
+(3, 1, 'Theard task', '333', '2018-08-05', 0, 0),
+(13, 1, '4th task', '444', '2018-08-06', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -59,14 +60,15 @@ CREATE TABLE IF NOT EXISTS `user` (
   `firstName` varchar(50) NOT NULL,
   `eMail` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
 -- Дамп данных таблицы `user`
 --
 
 INSERT INTO `user` (`id`, `login`, `password`, `firstName`, `eMail`) VALUES
-(1, 'sys', '111', 'Aleksei', 'Decurt_Gogencolern@mail.ru');
+(1, 'sys', '111', 'Aleksei', 'Decurt_Gogencolern@mail.ru'),
+(2, 'kk', '111', 'kk', 'kk@mail.ru');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
